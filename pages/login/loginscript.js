02b8617b542ab2b3b2169f794dad233090e28f1c
codeTun <iheblazhary@gmail.com>
2025-01-22 23:34:25 +0100

    ⚡ Going to prod !

--- pages/login/loginscript.js
+++ pages/login/loginscript.js
@@ -5,6 +5,8 @@ function validateLogin() {
   if (username === "gamingzone" && password === "gamingzone") {
     alert("User is connected");
     window.location.href = "../../user-interface/main.html";
+  } else if (username === "admin" && password === "admin") {
+    window.location.href = "../../admin-dashboard/index.html";
   } else {
     alert("Invalid username or password. Please try again.");
   }
